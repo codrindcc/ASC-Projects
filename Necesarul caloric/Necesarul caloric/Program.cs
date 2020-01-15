f3093aff65ac165fa7faee608d5d4801aa0ba546
codrindcc <56298801+codrindcc@users.noreply.github.com>
2020-01-15 15:52:28 +0200

    Add files via upload

---
+++ Necesarul caloric/Necesarul caloric/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Necesarul_caloric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti 0 daca sunteti femeie,respectiv 1 daca sunteti barbat: ");
+            int gen =int.Parse(Console.ReadLine());
+
+            //RMB Femei
+
+            if (gen == 0)
+            {
+                Console.Write("Introduceti greutatea dvs. in KG: ");
+                float greutate = float.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                Console.Write("Introduceti inaltimea dvs. in cm: ");
+                float inaltime = float.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                Console.Write("Introduceti varsta dvs.: ");
+                float varsta = float.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                float RMB = (float)655 + ((float)9.5 * greutate) + ((float)1.8 * inaltime) - ((float)4.7 * varsta);
+
+                Console.WriteLine("Rata metabolismului bazal este: " + RMB);
+
+                //Necesar caloric femei
+
+                Console.WriteLine("daca esti sedentar apasa 1");
+                Console.WriteLine("daca ai o activitate usoara apasa 2");
+                Console.WriteLine("daca ai o activitate moderata apasa 3");
+                Console.WriteLine("daca ai o activitate intensa apasa 4");
+                Console.WriteLine("daca ai o activitate foarte intensa apasa 5");
+
+                int activitate= int.Parse(Console.ReadLine());
+                float nc;
+
+                if(activitate==1)
+                {
+                    nc = RMB * (float)1.2;
+                    Console.Write("Necesarul dvs. caloric este: " +nc );
+                }
+
+                if (activitate == 2)
+                {
+                    nc = RMB * (float)1.375;
+                    Console.Write("Necesarul dvs. caloric este: " + nc);
+                }
+                if (activitate == 3)
+                {
+                    nc = RMB * (float)1.55;
+                    Console.Write("Necesarul dvs. caloric este: " + nc);
+                }
+                if (activitate == 4)
+                {
+                    nc = RMB * (float)1.725;
+                    Console.Write("Necesarul dvs. caloric este: " +nc );
+                }
+                if (activitate == 5)
+                {
+                    nc = RMB * (float)1.9;
+                    Console.Write("Necesarul dvs. caloric este: " +nc );
+                }
+
+            }
+
+            //RMB Barbati
+
+            else
+            {
+
+                Console.Write("Introduceti greutatea dvs. in KG: ");
+                float greutate1 = float.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                Console.Write("Introduceti inaltimea dvs. in cm: ");
+                float inaltime1 = float.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                Console.Write("Introduceti varsta dvs.: ");
+                float varsta1 = float.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                float RMB1 = (float)66 + ((float)13.7 * greutate1) + ((float)5 * inaltime1) - ((float)6.8 * varsta1);
+
+                Console.WriteLine("Rata metabolismului bazal este: " + RMB1);
+                
+                
+                //Necesar caloric barbati
+
+
+                Console.WriteLine("daca esti sedentar apasa 1");
+                Console.WriteLine("daca ai o activitate usoara apasa 2");
+                Console.WriteLine("daca ai o activitate moderata apasa 3");
+                Console.WriteLine("daca ai o activitate intensa apasa 4");
+                Console.WriteLine("daca ai o activitate foarte intensa apasa 5");
+
+                int activitate1= int.Parse(Console.ReadLine());
+                float nc1;
+
+                if (activitate1 == 1)
+                {
+                    nc1 = RMB1 * (float)1.2;
+                    Console.Write("Necesarul dvs. caloric este: " + nc1);
+                }
+
+                if (activitate1 == 2)
+                {
+                    nc1 = RMB1 * (float)1.375;
+                    Console.Write("Necesarul dvs. caloric este: " + nc1);
+                }
+                if (activitate1 == 3)
+                {
+                    nc1 = RMB1 * (float)1.55;
+                    Console.Write("Necesarul dvs. caloric este: " + nc1);
+                }
+                if (activitate1 == 4)
+                {
+                    nc1 = RMB1 * (float)1.725;
+                    Console.Write("Necesarul dvs. caloric este: " + nc1);
+                }
+                if (activitate1 == 5)
+                {
+                    nc1 = RMB1 * (float)1.9;
+                    Console.Write("Necesarul dvs. caloric este: " + nc1);
+                }
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ OPERATII_NR_MARI V2/OPERATII_NR_MARI/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPERATII_NR_MARI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OPERATII CU NR MARI
+
+            int[] v1 = new int[] { 3, 4, 4, 3, 3, 2, 1, 1, 1, 3, 4, 4, 6, 5, 5, 4, 3, 2, 3, 4, 4, 3, 3, 2, 1, 1, 1, 3, 4, 4, 6, 5, 5, 4, 3, 2, 3, 4, 4, 3, 3, 2, 1, 1, 1, 3, 4, 4, 6, 5, 5, 4, 3, 2, 3, 4, 4, 3, 3, 2, 1, 1, 1, 3, 4, 4, 6, 5, 5, 4, 3, 2 };
+            int[] v2 = new int[] { 9, 1, 1, 2, 3, 2, 2, 1, 2, 3, 4, 8, 8, 6, 2, 1, 1, 1, 3, 4, 4, 3, 3, 2, 1, 1, 1, 3, 4, 4, 6, 5, 5, 4, 3, 2, 3, 4, 4, 3, 3, 2, 1, 1, 1, 3, 4, 4, 6, 5, 5, 4, 3, 2, 3, 4, 4, 3, 3, 2, 1, 1, 1, 3, 4, 4, 6, 5, 5, 4, 3, 2, 3, 4, 4, 3, 3, 2, 1, 1, 1, 3, 4, 4, 6, 5, 5, 4, 3, 2 };
+
+            for (int i = 0; i < v1.Length; i++)
+                Console.Write(v1[i] + " ");
+            Console.WriteLine();
+            for (int i = 0; i < v2.Length; i++)
+                Console.Write(v2[i] + " ");
+            Console.WriteLine();
+
+            for (int i = 0; i < v1.Length / 2; i++)
+            {
+                int aux = v1[i];
+                v1[i] = v1[v1.Length - 1 - i];
+                v1[v1.Length - 1 - i] = aux;
+            }
+
+            for (int i = 0; i < v2.Length / 2; i++)
+            {
+                int aux = v2[i];
+                v2[i] = v2[v2.Length - 1 - i];
+                v2[v2.Length - 1 - i] = aux;
+            }
+
+            int n1 = v1.Length;
+            int n2 = v2.Length;
+
+            int max = n1;
+            if (n2 > n1) max = n2;
+
+            int[] v3 = new int[max];
+            int k1 = 0;
+            int k3 = 0;
+
+
+            int t = 0;
+            while (k1 < n1 && k1 < n2)
+            {
+                v3[k3] = (v1[k1] + v2[k1] + t) % 10;
+                t = (v1[k1] + v2[k1] + t) / 10;
+                k1++;
+                k3++;
+            }
+
+            while (k1 < n1)
+            {
+                v3[k3] = (v1[k1] + t) % 10;
+                t = (v1[k1] + t) / 10;
+                k1++;
+                k3++;
+
+            }
+            while (k1 < n2)
+            {
+                v3[k3] = (v2[k1] + t) % 10;
+                t = (v2[k1] + t) / 10;
+                k1++;
+                k3++;
+
+            }
+
+            if (t != 0)
+            {
+                int[] v4 = new int[max + 1];
+                for (int i = 0; i < max; i++)
+                    v4[i] = v3[i];
+                v4[max] = t;
+                v3 = v4;
+            }
+
+            for (int i = 0; i < v3.Length / 2; i++)
+            {
+                int aux = v3[i];
+                v3[i] = v3[v3.Length - 1 - i];
+                v3[v3.Length - 1 - i] = aux;
+            }
+
+
+            for (int i = 0; i < v1.Length; i++)
+                Console.Write(v1[i] + " ");
+            Console.WriteLine();
+            for (int i = 0; i < v2.Length; i++)
+                Console.Write(v2[i] + " ");
+            Console.WriteLine();
+            for (int i = 0; i < v3.Length; i++)
+                Console.Write(v3[i] + " ");
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
